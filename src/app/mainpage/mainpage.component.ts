0d624eb5b6f045fb4e741a2844123270ad556da1
WalterDoni <133245257+WalterDoni@users.noreply.github.com>
2024-04-18 21:30:59 +0200

    video function test

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -1,12 +1,43 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-mainpage',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './mainpage.component.html',
   styleUrl: './mainpage.component.scss'
 })
 export class MainpageComponent {
 
+  videos: any[] = []; // Renamed to `videos` for clarity
+
+  constructor() { }
+
+  async getVideos() {
+    const url = "http://127.0.0.1:8000/video/";
+    try {
+      const response = await fetch(url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json"
+        },
+      });
+      const data = await response.json();
+      this.videos = data.map((video: any) => ({
+        title: video.title,
+        description: video.description,
+        file: "http://127.0.0.1:8000" + video.video_file
+      }));
+      console.log(this.videos);
+    }
+    catch (e) {
+      console.log(e);
+    }
+  }
 }
+
+
+
+
+
