b0878924804d3758138d378697013d1634e33add
WalterDoni <133245257+WalterDoni@users.noreply.github.com>
2024-05-07 14:34:52 +0200

    start upload-component

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { HeaderMenuComponent } from '../fixed-menu-components/header-menu/header-menu.component';
+import { FooterComponent } from '../fixed-menu-components/footer/footer.component';
 
 @Component({
   selector: 'app-upload',
   standalone: true,
-  imports: [],
+  imports: [HeaderMenuComponent, FooterComponent],
   templateUrl: './upload.component.html',
   styleUrl: './upload.component.scss'
 })
 export class UploadComponent {
+  uploadedImg: any = {};
 
+  constructor() { }
+
+
+
+
+  uploadVideo(event: any) {
+    this.uploadedImg = event.target.files[0];
+
+  }
 }
