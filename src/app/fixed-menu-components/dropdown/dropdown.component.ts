78d40bd7959d94ffd8d983daceaa43e802d7142f
WalterDoni <133245257+WalterDoni@users.noreply.github.com>
2024-05-09 14:01:13 +0200

    profile

---
+++ src/app/fixed-menu-components/dropdown/dropdown.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-dropdown',
+  standalone: true,
+  imports: [],
+  templateUrl: './dropdown.component.html',
+  styleUrl: './dropdown.component.scss'
+})
+export class DropdownComponent {
+
+
+  constructor(private router: Router){
+
+  }
+
+
+  navigateToStartscreen(){
+    this.router.navigateByUrl('');
+  }
+}
--- src/app/fixed-menu-components/header-menu/header-menu.component.ts
+++ src/app/fixed-menu-components/header-menu/header-menu.component.ts
@@ -1,17 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { DropdownComponent } from '../dropdown/dropdown.component';
+
 
 @Component({
   selector: 'app-header-menu',
   standalone: true,
-  imports: [],
+  imports: [DropdownComponent],
   templateUrl: './header-menu.component.html',
   styleUrl: './header-menu.component.scss'
 })
 export class HeaderMenuComponent {
 
+  @ViewChild('dropdown')dropdown!: ElementRef;
+
   constructor(private router: Router) {
   }
+
+  toggleDropdown() {
+    const dropdownElement = this.dropdown.nativeElement;
+    if (dropdownElement.classList.contains("d-none")) {
+      dropdownElement.classList.remove("d-none");
+    } else {
+      dropdownElement.classList.add("d-none");
+    }
+  }
+
 
   navigateToUpload() {
     this.router.navigateByUrl('upload')
