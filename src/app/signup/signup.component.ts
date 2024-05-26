caaba67c7eb5354506225d971b1703af77c7b009
WalterDoni <133245257+WalterDoni@users.noreply.github.com>
2024-05-26 16:45:34 +0200

    signUp

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,22 +1,65 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
 
+  username: string = '';
+  email: string = '';
+  password: string = '';
+  checkPassword: string = '';
 
-  constructor(private router: Router){
+  constructor(private router: Router) {
 
   }
-  
+
+  createNewUser() {
+    if (this.password === this.checkPassword) {
+      this.createNewUserInBackend()
+    } else {
+      alert("Die Passwörter stimmen nicht überein!");
+
+    }
+  }
+
+  async createNewUserInBackend() {
+    const url = `http://127.0.0.1:8000/signup/`;
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: this.username,
+          email: this.email,
+          password: this.password,
+          phone: "-",
+          address: "-",
+          zipcode: "-",
+          city: "-",
+          country: "-",
+        }),
+      });
+      if (response.ok || response.status === 200) {
+        this.goToLoginPage();
+      }
+
+    } catch (e) {
+      alert("Bitte kontrollieren Sie nochmal Ihre Eingabe");
+    }
+  }
+
+
   goToLoginPage() {
     this.router.navigateByUrl('');
   }
-  
+
 }
