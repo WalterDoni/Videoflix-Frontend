228c2b7a2a972527c5b2a55cbac51e103728cfd4
WalterDoni <133245257+WalterDoni@users.noreply.github.com>
2024-05-28 17:27:28 +0200

    upload fix

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderMenuComponent } from '../fixed-menu-components/header-menu/header-menu.component';
 import { FooterComponent } from '../fixed-menu-components/footer/footer.component';
 
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-upload',
   standalone: true,
-  imports: [HeaderMenuComponent, FooterComponent],
+  imports: [HeaderMenuComponent, FooterComponent, FormsModule],
   templateUrl: './upload.component.html',
   styleUrl: './upload.component.scss'
 })
@@ -18,5 +20,15 @@ export class UploadComponent {
 
   constructor() { }
 
+  onFileChange(event: any) {
+    const fileInput = event.target as HTMLInputElement;
+    if (fileInput.files && fileInput.files.length > 0) {
+      this.file = fileInput.files[0];
+      console.log('Selected file:', this.file);
+    }
+  }
 
+  onSubmit(){
+    console.log("hello")
+  }
 }
