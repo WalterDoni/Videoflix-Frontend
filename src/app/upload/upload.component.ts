b549257f63eeb761edb729d0d84d7b0f3a04c83a
WalterDoni <133245257+WalterDoni@users.noreply.github.com>
2024-06-01 15:33:22 +0200

    upload videos

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { HeaderMenuComponent } from '../fixed-menu-components/header-menu/header-menu.component';
 import { FooterComponent } from '../fixed-menu-components/footer/footer.component';
-
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -28,7 +27,30 @@ export class UploadComponent {
     }
   }
 
-  onSubmit(){
-    console.log("hello")
+  async uploadNewMovie() {
+    debugger
+    const url = `http://127.0.0.1:8000/video/`;
+    const currentDate = new Date().toISOString().split('T')[0];
+
+    const formData = new FormData();
+    formData.append('created_at', currentDate);
+    formData.append('title', this.title);
+    formData.append('description', this.description);
+    formData.append('category', this.category);
+    formData.append('video_file', this.file);
+
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        body: formData,
+      });
+
+      if (response.ok || response.status === 200) {
+        console.log("Upload erfolgreich");
+      }
+    } catch (e) {
+      console.log("Fehler beim Upload:", e);
+    }
   }
+
 }
