c68ec1f68acc8215511b0479dd7a75911f237cb8
WalterDoni <133245257+WalterDoni@users.noreply.github.com>
2024-08-07 21:09:09 +0200

    div. changes

--- src/app/documentation/documentation.component.ts
+++ src/app/documentation/documentation.component.ts
@@ -1,17 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { HeaderMenuComponent } from '../fixed-menu-components/header-menu/header-menu.component';
 import { FooterComponent } from '../fixed-menu-components/footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-documentation',
   standalone: true,
-  imports: [HeaderMenuComponent, FooterComponent],
+  imports: [HeaderMenuComponent, FooterComponent, CommonModule],
   templateUrl: './documentation.component.html',
   styleUrl: './documentation.component.scss'
 })
 
 export class DocumentationComponent {
   videos: any[] = [];
+
+  @ViewChild('videoElement') videoElement!: ElementRef;
+  isDetailViewVisible = false;
+  selectedVideo: { file: string, title: string, description: string } = { file: '', title: '', description: '' };
+  videoName = "";
 
   constructor() {
     this.getVideos();
@@ -31,5 +37,36 @@ export class DocumentationComponent {
       file: 'https://videoflix-backend.walter-doni.at' + video.video_file,
     }));
   }
+
+
+  openVideoDetails(title: string, description: string, file: string) {
+    this.selectedVideo = { title, description, file };
+    const parsedUrl = new URL(file);
+    const path = parsedUrl.pathname;
+    const fileName = path.substring(path.lastIndexOf('/') + 1);
+    this.videoName = fileName.split('.')[0];
+    this.isDetailViewVisible = true;
+  }
+
+  ChangeVideoQuality(quality: string) {
+    const newSrc = `https://videoflix-backend.walter-doni.at/media/videos/${this.videoName}_${quality}.mp4`;
+    this.videoElement.nativeElement.src = newSrc;
+    this.videoElement.nativeElement.load();
+  }
+
+  ChangeVideoQualityStandard() {
+    const newSrc = `https://videoflix-backend.walter-doni.at/media/videos/${this.videoName}.mp4`;
+    this.videoElement.nativeElement.src = newSrc;
+    this.videoElement.nativeElement.load();
+  }
+
+  closeVideoDetails() {
+    this.isDetailViewVisible = false;
+  }
+
+  stopPropagation(event: Event) {
+    event.stopPropagation();
+  }
+
 }
 
--- src/app/series/series.component.ts
+++ src/app/series/series.component.ts
@@ -1,19 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { HeaderMenuComponent } from '../fixed-menu-components/header-menu/header-menu.component';
 import { FooterComponent } from '../fixed-menu-components/footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-series',
   standalone: true,
-  imports: [HeaderMenuComponent, FooterComponent],
+  imports: [HeaderMenuComponent, FooterComponent, CommonModule],
   templateUrl: './series.component.html',
   styleUrl: './series.component.scss',
 })
 export class SeriesComponent {
   videos: any[] = [];
 
+  @ViewChild('videoElement') videoElement!: ElementRef;
+  isDetailViewVisible = false;
+  selectedVideo: { file: string, title: string, description: string } = { file: '', title: '', description: '' };
+  videoName = "";
+
   constructor() {
-    this.getVideos()
+    this.getVideos();
   }
 
   async getVideos() {
@@ -29,5 +35,35 @@ export class SeriesComponent {
       category: video.category,
       file: 'https://videoflix-backend.walter-doni.at' + video.video_file,
     }));
+  }
+
+
+  openVideoDetails(title: string, description: string, file: string) {
+    this.selectedVideo = { title, description, file };
+    const parsedUrl = new URL(file);
+    const path = parsedUrl.pathname;
+    const fileName = path.substring(path.lastIndexOf('/') + 1);
+    this.videoName = fileName.split('.')[0];
+    this.isDetailViewVisible = true;
+  }
+
+  ChangeVideoQuality(quality: string) {
+    const newSrc = `https://videoflix-backend.walter-doni.at/media/videos/${this.videoName}_${quality}.mp4`;
+    this.videoElement.nativeElement.src = newSrc;
+    this.videoElement.nativeElement.load();
+  }
+
+  ChangeVideoQualityStandard() {
+    const newSrc = `https://videoflix-backend.walter-doni.at/media/videos/${this.videoName}.mp4`;
+    this.videoElement.nativeElement.src = newSrc;
+    this.videoElement.nativeElement.load();
+  }
+
+  closeVideoDetails() {
+    this.isDetailViewVisible = false;
+  }
+
+  stopPropagation(event: Event) {
+    event.stopPropagation();
   }
 }
